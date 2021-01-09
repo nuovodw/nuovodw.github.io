a6cb650ae7b28894fe318dd3f38389266de9c802
nuovodw <68261298+nuovodw@users.noreply.github.com>
2021-01-09 10:02:29 -0600

    Clean up script

--- Modal_Window/script.js
+++ Modal_Window/script.js
@@ -1,31 +1,22 @@
-//1. open modal window
-
-//1a. select the elements we need for DOM manipulations and assign a vairable to each class
-
 const openModalWindow = document.querySelectorAll('.show-modal'); //create NodeList
-console.log(openModalWindow); //this variable works
 const closeModalWindow = document.querySelector('.close-modal');
 const theModalWindow = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+const closeWindow = function () {
+  theModalWindow.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const openWindow = function () {
+  theModalWindow.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 //1b. Click the buttons
 
 for (i = 0; i < openModalWindow.length; i++) {
-  console.log(openModalWindow[i].textContent); // testing -- use textContent to show texts
-  openModalWindow[i].addEventListener('click', function () {
-    theModalWindow.classList.remove('hidden'); //no DOT!!
-    overlay.classList.remove('hidden');
-  });
+  openModalWindow[i].addEventListener('click', openWindow);
 }
 
 //2. Close modal window by a) clicking x b)cliking overlay
-
-closeModalWindow.addEventListener('click', function () {
-  theModalWindow.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
-
-overlay.addEventListener('click', function () {
-  theModalWindow.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+closeModalWindow.addEventListener('click', closeWindow);
+overlay.addEventListener('click', closeWindow);
